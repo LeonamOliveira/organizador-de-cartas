2713cf2e0b6d16ab6d038d0db3414a7d0c1f48a1
LeonamOliveira <oliveira.leonam@hotmail.com>
2024-09-30 23:08:35 -0300

    add react-icons and dialog

--- src/app/cards/data-table.tsx
+++ src/app/cards/data-table.tsx
@@ -1,78 +1,116 @@
 'use client'
-
-import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable
-} from '@tanstack/react-table'
-
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 import {
     Table,
     TableBody,
+    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
     TableRow
 } from '@/components/ui/table'
+import { FaPen, FaTrashCan } from "react-icons/fa6";
 
-interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
+enum CardGame{ 
+    Pokemon = 'Pokémon',
+    OnePiece = 'One Piece', 
+    YuGiOh = 'Yu-Gi-Oh!'
 }
 
-export function DataTable<TData, TValue>({
-    columns,
-    data,
-}: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-    })
+interface Card {
+    id: number;
+    quantidade: number;
+    nome: string;
+    cardgame: CardGame;
+}
+
+function CardList() {
+    const [cards, setCards] = useState<Card[]>([])
+
+    const handleEditCard = (cardId: number, newQuantity: number, newName: string, newCardgame: CardGame) => {
+        setCards(
+            cards.map((card) => 
+                card.id === cardId ? 
+                {...card, quantidade: newQuantity, nome: newName, cardgame: newCardgame} 
+                : card
+            )
+        )
+    }
+    
+}
+
+const cards = [
+    {
+        id: 8,
+        quantidade: 1,
+        cardgame: CardGame.YuGiOh,
+        nome: 'Mago Negro'
+    }, 
+    {
+        id: 7,
+        quantidade: 3,
+        cardgame: CardGame.Pokemon,
+        nome: 'Pikachu'
+    },
+    {
+        id: 12,
+        quantidade: 2,
+        cardgame: CardGame.OnePiece,
+        nome: 'Monkey D. Luffy'
+    }
+]
+
+export function DataTable() {
     return (
-        <div className='rounded-md border'>
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow  
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"}>
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+        <Table>
+            <TableCaption>Lista de cartas</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className='w-[250px]'>Quantidade</TableHead>
+                    <TableHead>CardGame</TableHead>
+                    <TableHead>Nome</TableHead>
+                    <TableHead>Ações</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                    cards.length > 0 ? (
+                        cards.map((card) => (
+                            <TableRow key={card?.id}>
+                                <TableCell>{card?.quantidade}</TableCell>
+                                <TableCell>{card?.cardgame}</TableCell>
+                                <TableCell>{card?.nome}</TableCell>
+                                <TableCell>
+                                    <div className='flex items-center gap-2'>
+                                        {/* <Button
+                                            variant='ghost'
+                                            size='icon'
+                                            onClick={() => handleEditCard(card.id, card.quantidade, card.nome, card.cardgame)}>
+                                        </Button> */}
+                                        <Button
+                                            variant='ghost'
+                                            size='icon'>
+                                                <FaPen />
+                                        </Button>
+                                        <Button
+                                            variant='ghost'
+                                            size='icon'>
+                                                <FaTrashCan />
+                                        </Button>
+                                    </div>
+                                </TableCell>
                             </TableRow>
                         ))
                     ) : (
                         <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                Sem resultado.
+                            <TableCell>
+                                Não encontrado
                             </TableCell>
                         </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </div>
+                    )
+                }
+            </TableBody>
+        </Table>
     )
 }
--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,23 +1,9 @@
-import { columns, Cards } from "./columns";
 import { DataTable } from "./data-table";
 
-async function getData(): Promise<Cards[]> {
-    return [
-        {
-            id: '1231321',
-            typeOfCard: 'Pokémon',
-            name: 'Pikachu',
-            quantity: 1
-        }
-    ]
-}
-
 export default async function DemoPage() {
-    const data = await getData()
-
     return (
         <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={data} />
+            <DataTable/>
         </div>
     )
 }
