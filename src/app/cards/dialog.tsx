9d85b3b9f4b473c695ac2a4cbf744af3d0a80424
LeonamOliveira <oliveira.leonam@hotmail.com>
2024-10-14 09:24:21 -0300

    add dropdown to see cardgame options and label if edit card

--- src/app/cards/dialog.tsx
+++ src/app/cards/dialog.tsx
@@ -3,14 +3,23 @@ import {
   Dialog,
   DialogContent,
   DialogHeader,
-  DialogTitle,
-  DialogTrigger
+  DialogTitle
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { DialogEditProps } from './dialog.interface';
 import { useState } from 'react';
 import { CardGame } from './card.interface';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 export function DialogEdit({ title, isOpen, onClose, card, onSubmit}: DialogEditProps) {
   const [quantidade, setQuantidade] = useState(card?.quantidade);
@@ -43,11 +52,7 @@ export function DialogEdit({ title, isOpen, onClose, card, onSubmit}: DialogEdit
             onChange={(e) => setNome(e.target.value)}
           />
           <Label htmlFor='cardgame'>CardGame</Label>
-          <Input 
-            id='cardgame'
-            value={cardgame}
-            onChange={(e) => setCardGame(e.target.value as CardGame)}
-            />
+          <p>{cardgame}</p>
         </div>
         <Button style={{backgroundColor: 'green', color: 'white'}} onClick={handleSubmit}>Salvar</Button>
       </DialogContent>
@@ -84,3 +89,14 @@ export const DialogConfirm: React.FC<DialogConfirmProps> = ({
   </Dialog>
   )
 }
+
+{/* <DropdownMenu>
+  <DropdownMenuTrigger>{cardgame || 'Selecione um CardGame'}</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>CardGames</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem onClick={() => setCardGame(CardGame.YuGiOh)}>Yu-Gi-Oh!</DropdownMenuItem>
+    <DropdownMenuItem onClick={() => setCardGame(CardGame.Pokemon)}>Pokémon</DropdownMenuItem>
+    <DropdownMenuItem onClick={() => setCardGame(CardGame.OnePiece)}>One Piece</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu> */}
